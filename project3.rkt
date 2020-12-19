b398149f6b202944d1e4efa6e5d89c7811806594
milochase13 <milochase13@gmail.com>
2020-12-19 15:44:29 -0500

    Update project3.rkt

--- project3.rkt
+++ project3.rkt
@@ -4,7 +4,6 @@
 (require "../include/cs151-image.rkt")
 (require "../include/cs151-universe.rkt")
 
-;; We'll use the same version of Some and Optional that we used on Homework 5.
 (define-struct (Some X)
   ([value : X]))
     
